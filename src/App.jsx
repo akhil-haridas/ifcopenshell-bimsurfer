697fae4c6d5e7d834c86ef9a3aa079b4e81de29e
akhil-haridas <4khilharidas@gmail.com>
2024-08-09 19:43:04 +0530

    updated view for the panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [bimSurferLoaded, setBimSurferLoaded] = useState(false);
+  const viewerRef = useRef(null);
+
+  useEffect(() => {
+    const initializeBimSurfer = () => {
+      if (window.BimSurfer && viewerRef.current) {
+        const bimSurfer = new window.BimSurfer({
+          domNode: "viewerContainer",
+        });
+        return () => {
+          bimSurfer.destroy();
+        };
+      } else {
+        console.error(
+          "BimSurfer is not available or not initialized correctly."
+        );
+      }
+    };
+
+    const pollForBimSurfer = () => {
+      if (window.BimSurfer && typeof window.BimSurfer === "function") {
+        setBimSurferLoaded(true);
+      } else {
+        setTimeout(pollForBimSurfer, 100);
+      }
+    };
+
+    pollForBimSurfer();
+
+    if (bimSurferLoaded) {
+      initializeBimSurfer();
+    }
+  }, [bimSurferLoaded]);
+
   return (
-    <div id="viewerContainer" style={{ width: "100%", height: "100vh" }}>
-      Hello World
-    </div>
+    <div
+      id="viewerContainer"
+      ref={viewerRef}
+      style={{ width: "100%", height: "100vh" }}
+    ></div>
   );
 }
 
