63eca8a20e1ef02aed907e5d6624fc45215845cf
akhil-haridas <4khilharidas@gmail.com>
2024-08-12 15:32:46 +0530

    using the BimViewer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,58 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import "./App.css";
 import BimViewer from "./components/BimViewer";
+import UploadFile from "./components/UploadFile";
 
 function App() {
-  // const [bimSurferLoaded, setBimSurferLoaded] = useState(false);
-  // const viewerRef = useRef(null);
-
-  // console.log(window.BimSurfer)
-  // useEffect(() => {
-  //   const initializeBimSurfer = () => {
-  //     if (window.BimSurfer && viewerRef.current) {
-  //       const bimSurfer = new window.BimSurfer({
-  //         domNode: "viewerContainer",
-  //       });
-
-  //       bimSurfer
-  //         .load({
-  //           src: "/models/Duplex",
-  //           // src: "/models/SampleGLB",
-  //           // type: "gltf",
-  //         })
-  //         .then(() => {
-  //           console.log("Model loaded successfully.");
-  //         })
-  //         .catch((error) => {
-  //           console.error("Error loading model:", error);
-  //         });
-
-  //       return () => {
-  //         bimSurfer.destroy();
-  //       };
-  //     } else {
-  //       console.error(
-  //         "BimSurfer is not available or not initialized correctly."
-  //       );
-  //     }
-  //   };
-
-  //   const pollForBimSurfer = () => {
-  //     if (window.BimSurfer && typeof window.BimSurfer === "function") {
-  //       setBimSurferLoaded(true);
-  //     } else {
-  //       setTimeout(pollForBimSurfer, 100);
-  //     }
-  //   };
-
-  //   pollForBimSurfer();
-
-  //   if (bimSurferLoaded) {
-  //     initializeBimSurfer();
-  //   }
-  // }, [bimSurferLoaded]);
-
-  return <BimViewer />;
+  return (
+    // <div className="BimViewerWrapper">
+    //   <UploadFile />
+      <BimViewer />
+    // {/* </div> */}
+  );
 }
 
 export default App;
