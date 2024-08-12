4e1ad4d34ac01de5bb3c4a2a7fd7cba203a91c15
akhil-haridas <4khilharidas@gmail.com>
2024-08-12 11:54:59 +0530

    component for the bimviewer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,63 +1,58 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
+import BimViewer from "./components/BimViewer";
 
 function App() {
-  const [bimSurferLoaded, setBimSurferLoaded] = useState(false);
-  const viewerRef = useRef(null);
+  // const [bimSurferLoaded, setBimSurferLoaded] = useState(false);
+  // const viewerRef = useRef(null);
 
-  console.log(window.BimSurfer)
-  useEffect(() => {
-    const initializeBimSurfer = () => {
-      if (window.BimSurfer && viewerRef.current) {
-        const bimSurfer = new window.BimSurfer({
-          domNode: "viewerContainer",
-        });
+  // console.log(window.BimSurfer)
+  // useEffect(() => {
+  //   const initializeBimSurfer = () => {
+  //     if (window.BimSurfer && viewerRef.current) {
+  //       const bimSurfer = new window.BimSurfer({
+  //         domNode: "viewerContainer",
+  //       });
 
-        bimSurfer
-          .load({
-            src: "/models/Duplex",
-            // src: "/models/SampleGLB",
-            // type: "gltf",
-          })
-          .then(() => {
-            console.log("Model loaded successfully.");
-          })
-          .catch((error) => {
-            console.error("Error loading model:", error);
-          });
+  //       bimSurfer
+  //         .load({
+  //           src: "/models/Duplex",
+  //           // src: "/models/SampleGLB",
+  //           // type: "gltf",
+  //         })
+  //         .then(() => {
+  //           console.log("Model loaded successfully.");
+  //         })
+  //         .catch((error) => {
+  //           console.error("Error loading model:", error);
+  //         });
 
-        return () => {
-          bimSurfer.destroy();
-        };
-      } else {
-        console.error(
-          "BimSurfer is not available or not initialized correctly."
-        );
-      }
-    };
+  //       return () => {
+  //         bimSurfer.destroy();
+  //       };
+  //     } else {
+  //       console.error(
+  //         "BimSurfer is not available or not initialized correctly."
+  //       );
+  //     }
+  //   };
 
-    const pollForBimSurfer = () => {
-      if (window.BimSurfer && typeof window.BimSurfer === "function") {
-        setBimSurferLoaded(true);
-      } else {
-        setTimeout(pollForBimSurfer, 100);
-      }
-    };
+  //   const pollForBimSurfer = () => {
+  //     if (window.BimSurfer && typeof window.BimSurfer === "function") {
+  //       setBimSurferLoaded(true);
+  //     } else {
+  //       setTimeout(pollForBimSurfer, 100);
+  //     }
+  //   };
 
-    pollForBimSurfer();
+  //   pollForBimSurfer();
 
-    if (bimSurferLoaded) {
-      initializeBimSurfer();
-    }
-  }, [bimSurferLoaded]);
+  //   if (bimSurferLoaded) {
+  //     initializeBimSurfer();
+  //   }
+  // }, [bimSurferLoaded]);
 
-  return (
-    <div
-      id="viewerContainer"
-      ref={viewerRef}
-      style={{ width: "100%", height: "100vh" }}
-    ></div>
-  );
+  return <BimViewer />;
 }
 
 export default App;
